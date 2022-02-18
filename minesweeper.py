6bf8995f4cc90e2d5f596c8ffdcf4e918fa07be5
Veronica Conesa <vero.gconesa@gmail.com>
2022-02-18 23:30:00 +0100

    Created grid and user grid

---
+++ minesweeper.py
@@ -0,0 +1,80 @@
+import os
+
+
+columns=0
+rows=0
+mines=0
+
+#FUNCTIONS
+
+def game_presentation():
+    
+    os.system("cls")
+  
+    print("Welcome to Minesweeper!!")
+    print("Let's see if you can find all the mines. Are your ready??")
+    print()
+    print("Let's create the boardgame...")   
+    col = int(input("Please enter the number of columns: "))
+    row = int(input("Now enter the number of rows: "))    
+    bomb = int(input("And now, how many mines do you want to find? "))
+    return col, row, bomb
+    
+    
+def instructions():
+    
+    os.system("cls")
+    print("     To move:")
+    print("             Up:    w")
+    print("             Down:  s")
+    print("             Right: d")
+    print("             Left:  a")
+    print()
+    print("     To open the cell:")
+    print("             Open:   o")
+    print()
+    print("     To flag a mine:")
+    print("             Flag:   f")
+    print()
+    print("Press enter to start...")
+    print()
+
+
+def menu():
+    print()
+    print("Enter your move: ")
+    selected = input("w/s/a/d - o - f ?")
+    return selected
+
+def creating_grid(columns, rows, val):
+    grid=[]
+    for i in range(rows):
+        grid.append([])
+        for j in range(columns):
+            grid[i].append(val)
+    return grid
+    
+
+def show_grid():
+    pass
+
+def adding_mines():
+    pass
+
+def place_flag():
+    pass
+
+#Testing 
+
+columns, rows, mines = game_presentation()
+instructions()
+
+print(columns)
+print(rows)
+print(mines)
+
+grid= creating_grid(columns, rows, 0)
+print(grid)
+
+user_grid = creating_grid(columns, rows, "-")
+print(user_grid)
